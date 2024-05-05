a479b06c7f29da5174eded0b632bd75ba2cd1912
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2024-05-05 13:30:20 -0300

    feat: Add spawner
    Created a spawner with random spawn rate and different spawn chances for each prefab

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,50 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    
+    [System.Serializable]
+    public struct SpawnableObject
+    { 
+        public GameObject prefab;
+        [Range(0f, 1f)] //Makes a slider in Unity
+        public float spawnChance;
+    }
+
+    public SpawnableObject[] objects;
+
+    //Sets min and max spawn rate
+    public float minSpawnRate = 1f;
+    public float maxSpawnRate = 2f;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn()
+    {
+        var spawnChance = Random.value;
+
+        foreach (var obj in objects)
+        {
+            if (spawnChance < obj.spawnChance)
+            {
+                GameObject objectPrefab =  Instantiate(obj.prefab);
+                objectPrefab.transform.position += transform.position;
+                break;
+            }
+
+            spawnChance -= obj.spawnChance;
+        }
+        
+        //Spawns another object with a new spawn rate
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
 }
